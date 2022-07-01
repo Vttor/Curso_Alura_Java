392eed15389efc474bed223f2b850be6844a9d30
Vttor <52765127+Vttor@users.noreply.github.com>
2022-07-01 18:50:19 -0300

    Aulas 4 e 5

---
+++ Sintaxe_Variaveis_e_Fluxo/TestaVariaveis/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Olá novo teste");
+        int idade = 37;
+        System.out.println("A idade é " + idade + ", parábens!");
+        
+    }
+}
---
+++ Sintaxe_Variaveis_e_Fluxo/TestaVariaveis/src/TesteConversao.java
@@ -0,0 +1,15 @@
+public class TesteConversao {
+    public static void main(String[] args) {
+        double salario = 1270.50;
+        int valor = (int) salario;
+        System.out.println(valor);
+
+        float pontoFlutuante = 3.14f;
+        long numeroGrande = 3251642212l;
+        
+        double valor1 = 0.2, valor2 = 0.1;
+        double total = valor1 + valor2;
+        System.out.println(total);
+
+    }
+}
